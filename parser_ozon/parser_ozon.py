2a47555bd281b2405de66321dfcf41204c18170d
Mikhail Murashev <mihail.mmm@inbox.ru>
2023-02-02 20:56:33 +0300

    Добавлен сбор url товаров и их сохранение в csv файл пользователя.

--- parser_ozon/parser_ozon.py
+++ parser_ozon/parser_ozon.py
@@ -1,6 +1,5 @@
 from parser_ozon.utils_parser import product_filter, price_filter, product_amount_edit
 
-from dramatiq.brokers.redis import RedisBroker 
 from parser_ozon import xpath_ozon
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -10,7 +9,6 @@ from selenium.common.exceptions import NoSuchElementException
 from parser_ozon.settings_parser import CHR_PATH, USER_DATA_PATH
 
 import csv
-import dramatiq
 import time
 
 class Parser:
@@ -20,6 +18,7 @@ class Parser:
         self.chat_id = chat_id
         self.product_carts = []
         self.price_carts = []
+        self.product_url = []
         self.parsing_result = []
 
     def parser_ozon(self):
@@ -62,6 +61,19 @@ class Parser:
                     break
             return self.price_carts
 
+        def find_url():
+            for i in range(1, 36):
+                for xpath in xpath_ozon.XPATH_LIST_NAME:
+                    new_xpath = xpath + f'[{i}]' + '/div[2]/div/a'
+                    try:
+                        url = driver.find_element(By.XPATH, new_xpath)
+                        url = url.get_attribute('href')
+                    except NoSuchElementException:
+                        continue
+                    self.product_url.append(url)
+                    break
+            return self.product_url
+
         while True:
             try:
                 options = Options()
@@ -100,7 +112,7 @@ class Parser:
         driver.quit()
 
         options = Options()
-        driver = webdriver.Chrome(chrome_options=options, executable_path="./chromedriver/chromedriver")
+        driver = webdriver.Chrome(chrome_options=options, executable_path=CHR_PATH)
 
         driver.get(current_url)
 
@@ -108,7 +120,9 @@ class Parser:
 
         self.product_carts = product_carts_info()
         self.price_carts = price_carts_info()
+        self.product_url = find_url()
 
+        product_url = self.product_url
         product_names = product_filter(self.product_carts)[0]
         rating_list = product_filter(self.product_carts)[1]
         reviews_amount = product_filter(self.product_carts)[2]
@@ -124,7 +138,7 @@ class Parser:
             self.product_carts = []
             self.price_carts = []
 
-            next_url = current_url + f'?page={i}'
+            next_url = current_url + f'?page={i}&tf_state=sdfjkhskjdhfkjshf'
             options = Options()
             driver = webdriver.Chrome(chrome_options=options, executable_path=CHR_PATH)
             driver.get(next_url)
@@ -132,7 +146,9 @@ class Parser:
 
             self.product_carts = product_carts_info()
             self.price_carts = price_carts_info()
+            self.product_url = find_url()
 
+            product_url.extend(self.product_url)
             product_names.extend(product_filter(self.product_carts)[0])
             rating_list.extend(product_filter(self.product_carts)[1])
             reviews_amount.extend(product_filter(self.product_carts)[2])
@@ -145,16 +161,17 @@ class Parser:
         # print(reviews_amount)
         # print(prices_with_card)
         # print(prices_without_card)
-        print(len(product_names))
-        print(len(rating_list))
-        print(len(reviews_amount))
-        print(len(prices_with_card))
-        print(len(prices_without_card))
-        print(product_amount)
+        # print(len(product_names))
+        # print(len(rating_list))
+        # print(len(reviews_amount))
+        # print(len(prices_with_card))
+        # print(len(prices_without_card))
+        # print(product_amount)
 
         for i in range(0, (product_amount - 1)):
             product_data = {}
             product_data['product_name'] = product_names[i]
+            product_data['url'] = product_url[i]
             product_data['rating'] = rating_list[i]
             product_data['amount_review'] = reviews_amount[i]
             product_data['price_with_card'] = prices_with_card[i]
@@ -163,7 +180,7 @@ class Parser:
         return self.parsing_result
 
     def seve_data(self):
-        csv_colums = ['product_name', 'rating', 'amount_review', 'price_with_card', 'price_without_card']
+        csv_colums = ['product_name', 'url', 'rating', 'amount_review', 'price_with_card', 'price_without_card']
         save_path = f'{USER_DATA_PATH}user_{self.chat_id}.csv'
         with open(save_path, 'w', encoding='utf-8') as f:
             writer = csv.DictWriter(f, fieldnames=csv_colums)
