5722431f390c1d27f655e33cc4b2c881bff4bec8
Mikhail <81187343+mikhail-mmm@users.noreply.github.com>
2023-02-02 20:57:39 +0300

    Добавлен сбор url товаров и их сохранение в csv файл пользователя. (#4)

--- start_parsing.py
+++ start_parsing.py
@@ -1,11 +1,14 @@
-import dramatiq
-from dramatiq.brokers.rabbitmq import RabbitmqBroker
+# import dramatiq
+# from dramatiq.brokers.rabbitmq import RabbitmqBroker
 from parser_ozon.parser_ozon import Parser
 
-dramatiq.set_broker(RabbitmqBroker())
+# dramatiq.set_broker(RabbitmqBroker())
 
-@dramatiq.actor
+# @dramatiq.actor
 def start_parser(chat_id, user_request):
     parser = Parser(chat_id, user_request)
     parser.parser_ozon()
     parser.seve_data()
+
+if __name__ == '__main__':
+    start_parser('123321', '3D принтер')
